a43c6f8d3f9ffe6ae19c23c6bb64a6fc93fc78c5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-14 00:59:06 -0500

    initial

---
+++ src/largest-semantic-change-spec.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/* global describe, it */
+describe('largest-semantic-change', () => {
+  const largestSemanticChange = require('.')
+  it('write this test', () => {
+    console.assert(largestSemanticChange, 'should export something')
+  })
+})
